0c730e0be846f8b25acffdc005c42fad14682a12
Christopher Bryan <christopher_bryan@rocketmail.com>
2021-05-23 18:48:39 +0700

    'g'

--- bot.py
+++ bot.py
@@ -134,7 +134,7 @@ async def play(ctx, *url):
         while voice.is_playing():  # Checks if voice is playing
             await asyncio.sleep(1)  # While it's playing it sleeps for 1 second
         else:
-            await asyncio.sleep(15)  # If it's not playing it waits 15 seconds
+            await asyncio.sleep(5)  # If it's not playing it waits 15 seconds
             while voice.is_playing():  # and checks once again if the bot is not playing
                 break  # if it's playing it breaks
             else:
