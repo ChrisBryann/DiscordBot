9777cd86c09fc862a7a6a21c0066db34cd94b73f
Christopher Bryan <christopher_bryan@rocketmail.com>
2021-05-30 23:33:43 +0700

    'g'

--- bot.py
+++ bot.py
@@ -13,10 +13,12 @@ import giphy_client
 from giphy_client.rest import ApiException
 from dotenv import load_dotenv
 from discord.ext import commands
+from pybooru import Danbooru
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+DANBOORU = Danbooru('danbooru')
 word_headers = {
     'x-rapidapi-key': "1cbdad4150msh72e9b55eed74ad8p1fad8cjsn55ddaa3526f6",
     'x-rapidapi-host': "wordsapiv1.p.rapidapi.com"
